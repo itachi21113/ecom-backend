7a11e4f30327b10453f3fde1da3b04780e73e6e5
Utkarsh <your-email@example.com>
2025-08-22 07:17:16 +0530

    Nothing just checking

--- ecom-backend/src/main/java/com/example/ecommerce/ecom_backend/config/SecurityConfig.java
+++ ecom-backend/src/main/java/com/example/ecommerce/ecom_backend/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         // This is the address of your front-end
-        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500", "http://localhost:5500" , "http://127.0.0.1:5503"));
+        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500", "http://localhost:5500" ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
